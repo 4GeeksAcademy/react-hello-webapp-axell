3f2334ba066cf85cd44bf0ff637184be7821e27f
axelluribe <axelluribe13@gmail.com>
2025-10-08 08:31:57 +0000

    First Commit

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useGlobalReducer } from "../hooks/useGlobalReducer";
 import { ContactCard } from "../components/ContactCard";
@@ -6,11 +6,36 @@ import { ContactCard } from "../components/ContactCard";
 export const Contact = () => {
   const { store, dispatch } = useGlobalReducer();
 
+  
+ const createAgenda = async () => {
+  try {
+    const resp = await fetch(
+      "https://playground.4geeks.com/contact/agendas/axelluribe",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({})
+      }
+    );
+
+    if (resp.status === 201) {
+      console.log("Agenda creada correctamente");
+    } else if (resp.status === 400) {
+      console.log("La agenda ya existe ");
+    } else {
+      console.error("Error al crear agenda:", resp.status);
+    }
+  } catch (err) {
+    console.error("Error en createAgenda:", err);
+  }
+};
+  
   const getContacts = async () => {
     try {
       const resp = await fetch(
         "https://playground.4geeks.com/contact/agendas/axelluribe/contacts"
       );
+
       const data = await resp.json();
       dispatch({ type: "SET_CONTACTS", payload: data.contacts || [] });
     } catch (err) {
@@ -19,7 +44,11 @@ export const Contact = () => {
   };
 
   useEffect(() => {
-    getContacts();
+    const init = async () => {
+      await createAgenda(); 
+      await getContacts();  
+    };
+    init();
   }, []);
 
   return (
